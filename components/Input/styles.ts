90252f2935e1f2d2c23c115a02a32b99c6631aa5
Israel Lana <61506375+israellana@users.noreply.github.com>
2022-10-19 08:51:56 -0300

    Update styles.ts

--- components/Input/styles.ts
+++ components/Input/styles.ts
@@ -10,13 +10,28 @@ const styles = {
         "focus:border-solid",
         "focus:outline-none",
         "focus:border-2",
-        "block focus:outline-none focus:ring-0 peer"
+        "block", 
+        "focus:outline-none", 
+        "focus:ring-0 peer"
     ].join(' '),
     label: () => [
         "ml-6",
         "mt[6px]",
         "text-xs",
-        "absolute duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] left-2.5 peer-focus:text-primary-300 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4"
+        "absolute",
+        "duration-300",
+        "transform",
+        "-translate-y-4",
+        "scale-75",
+        "top-4",
+        "z-10",
+        "origin-[0]",
+        "left-2.5",
+        "peer-focus:text-primary-300",
+        "peer-placeholder-shown:scale-100",
+        "peer-placeholder-shown:translate-y-0",
+        "peer-focus:scale-75",
+        "peer-focus:-translate-y-4"
     ].join(' '),
     container: () => [
         "relative",
