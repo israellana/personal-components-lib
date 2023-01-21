62eaae1c6d149a78cbab78d11dee1201130cf62a
Israel Lana <61506375+israellana@users.noreply.github.com>
2023-01-21 16:11:36 -0300

    Remove unnecessary import

--- components/Spacer/index.tsx
+++ components/Spacer/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import classes from './styles'
 
 interface SpacerProps{
   size: string;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import Typography from '../components/Typography'
 
 export default function Home() {
   return (
