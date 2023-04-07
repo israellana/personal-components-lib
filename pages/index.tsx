15fcce3f6b5113fe53f22cdd674c3fac5b43acea
Israel Lana <61506375+israellana@users.noreply.github.com>
2023-04-07 11:25:03 -0300

    pages back

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Landing | Undonkey VR</title>
+        <link rel="icon" href="/favicon.svg" />
+      </Head>
+
+      <main className="flex items-center justify-center h-screen w-screen">
+        Teste
+      </main>
+    </div>
+  )
+}
